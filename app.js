ecf8fd7f592801f01d77287e25fa782120d77cae
fsuffieldcode <fsuffieldcode@gmail.com>
2020-02-06 16:53:49 +0000

    restore spotify api and .env

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose")
 const session = require("express-session")
 const passport = require("passport")
 const passportLocalMongoose = require("passport-local-mongoose")
+const port = process.env.PORT || 3000;
 
 const app = express()
 
@@ -27,7 +28,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 
-mongoose.connect("mongodb://localhost:27017/tennersDB", {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb://localhost:27017/tennersDB", { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
@@ -40,9 +41,38 @@ userSchema.plugin(passportLocalMongoose)
 const User = mongoose.model("User", userSchema)
 
 passport.use(User.createStrategy());
- 
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+
+
+// SPOTIFY API INITIALISATION HERE
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: 'https://spotlify.herokuapp.com' || 'https://localhost:3000'
+});
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant().then(
+    function (data) {
+        console.log('The access token expires in ' + data.body['expires_in']);
+        console.log('The access token is ' + data.body['access_token']);
+
+        // Save the access token so that it's used in future calls
+        spotifyApi.setAccessToken(data.body['access_token']);
+    },
+    function (err) {
+        console.log('Something went wrong when retrieving an access token', err);
+    }
+);
+
+
+
+//  GET ROUTES
+
 
 app.get("/", function (req, res) {
     res.render("landing")
@@ -57,21 +87,32 @@ app.get("/register", function (req, res) {
 })
 
 app.get("/home", function (req, res) {
-    if (req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         res.render("home")
     } else {
         res.redirect("/login")
     }
 })
 
+app.get("/add", function (req, res) {
+    res.render('search')
+})
+
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect('/');
+})
+
+// POST ROUTES
+
 app.post("/register", function (req, res) {
 
-    User.register({username: req.body.username}, req.body.password, function(err, user){
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
         if (err) {
             console.log(err)
             res.redirect("/register")
         } else {
-            passport.authenticate("local")(req, res, function(){
+            passport.authenticate("local")(req, res, function () {
                 res.redirect("/home")
             })
         }
@@ -86,27 +127,18 @@ app.post("/login", function (req, res) {
         password: req.body.password
     })
 
-    req.login(user, function(err){
+    req.login(user, function (err) {
         if (err) {
             console.log(err)
         } else {
-            passport.authenticate("local")(req, res, function(){
+            passport.authenticate("local")(req, res, function () {
                 res.redirect("/home")
             })
         }
     })
 })
 
-app.get("/add", function (req, res) {
-    res.render('search')
-})
 
-app.get("/logout", function (req, res) {
-    req.logout();
-    res.redirect('/');
-})
 
- 
-app.listen(3000, function() {
-    console.log("Server started on port 3000")
-})
+
+app.listen(port, () => console.log('My Spotify project running on port ' + port + ' 🎧 🥁 🎸 🔊'));
